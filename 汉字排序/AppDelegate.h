b9c0d4675b166d251ba9bc88270c80e1c3f1ff22
我 <KZ@zhangkundemacbook-pro.local>
2015-12-25 15:08:46 +0800

    Initial Commit

---
+++ 汉字排序/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  汉字排序
+//
+//  Created by 张坤 on 15/12/25.
+//  Copyright © 2015年 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 汉字排序/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  汉字排序
+//
+//  Created by 张坤 on 15/12/25.
+//  Copyright © 2015年 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
